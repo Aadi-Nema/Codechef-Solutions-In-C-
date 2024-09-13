c957eb2aeee196f684dff421269464d6710dd07a
Aadi-Nema <aadi.nema.cs21@ggits.net>
2024-09-13 19:47:50 +0530

    Add files via upload

---
+++ Christmas.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	if(x==25)
+	{
+	    cout<<"CHRISTMAS"<<endl;
+	}
+	
+	else
+	{
+	    cout<<"ORDINARY"<<endl;
+	}
+
+}
