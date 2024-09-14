5d752ffc40054f4b1a318e87a961696c823a0e0d
Aadi-Nema <aadi.nema.cs21@ggits.net>
2024-09-14 20:06:06 +0530

    Add files via upload

---
+++ Codechef_Round.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+   
+        int x;
+        cin>>x;
+        if(x==4)
+        {
+            cout<<"YES"<<endl;
+        }
+        
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    
+
+}
